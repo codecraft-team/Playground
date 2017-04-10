64b93f1dfeb61c234c5788c4d4fc696350baa0f7
Giuseppe Calandriello <giuseppe.calandriello@thinkject.de>
2017-04-10 08:04:55 +0200

    Imp: Add scrollIntoView functionality

--- AngularTreeView/app/app.ts
+++ AngularTreeView/app/app.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from "@angular/core";
-import { TreeNode } from "./treeNode";
+import { Component, OnInit } from "@angular/core";
+import { TreeNode } from "./treeNode";
 
 @Component({
   moduleId: module.id,
-  selector: "my-app",
+  selector: "my-app",
   template: `
   <div>Selected node: {{selectedNode?.name}}</div>
   <br/>
-  <treenode [nodes]="nodes" (selected)="select($event)"></treenode>`
+  <button (click)='scroll("1.19")'>scroll to 1.19</button>
+  <div style="overflow-y: scroll; height: 300px;">
+    <treenode [nodes]="nodes" (selected)="select($event)"></treenode>
+  </div>
+  `
 })
-export  class  AppComponent implements OnInit {
+export class AppComponent implements OnInit {
   nodes: TreeNode[];
   selectedNode: TreeNode;
 
   ngOnInit(): void {
     let node1 = new TreeNode("1");
-    node1.nodes.push(new TreeNode("1.1"));
-    node1.nodes.push(new TreeNode("1.2"));
-    node1.nodes.push(new TreeNode("1.3"));
+    for (var index = 0; index < 20; index++) {
+      node1.nodes.push(new TreeNode(`1.${index}`));
+    }
 
     let node21 = new TreeNode("2.1");
-    node21.nodes.push(new TreeNode("2.1.1"));
+    for (var index = 0; index < 20; index++) {
+      node21.nodes.push(new TreeNode(`2.1.${index}`));
+    }
 
     let node2 = new TreeNode("2");
     node2.nodes.push(node21);
@@ -35,5 +41,12 @@ export  class  AppComponent implements OnInit {
 
   select(node: TreeNode) {
     this.selectedNode = node;
+  }
+
+  scroll(id: string) {
+    let treenode = document.getElementById(id);
+    if (treenode != undefined) {
+      treenode.scrollIntoView(true);
+    }
   }
 };
