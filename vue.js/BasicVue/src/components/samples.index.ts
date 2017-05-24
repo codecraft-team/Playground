ff8d9c6c47ded3eb5663b26cfad939443c4ae87a
Joachim Jauß <joachim.jauss@outlook.de>
2017-05-24 12:29:23 +0200

    basicVue: add a Vue plugin which registers a filter in the current vue instance

--- vue.js/BasicVue/src/components/samples.index.ts
+++ vue.js/BasicVue/src/components/samples.index.ts
@@ -2,10 +2,12 @@ import { PlainComponent } from './samples/plainComponent';
 import { VueClassComponent } from './samples/vueClassComponent';
 import { VuePropertyComponent } from './samples/vuePropertyDecoratedComponent';
 import { ValueFormattingComponent } from './samples/valueFormattingComponent';
+import { PluginUsageComponent } from './samples/plugin/pluginUsageComponent';
 
 export let samples = {
   'plain': PlainComponent,
   'vueClass': VueClassComponent,
   'vueProperty-Class': VuePropertyComponent,
-  'formatting': ValueFormattingComponent
+  'formatting': ValueFormattingComponent,
+  'plugin': PluginUsageComponent
 }
