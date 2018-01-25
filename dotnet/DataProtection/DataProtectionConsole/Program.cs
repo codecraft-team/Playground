83ebd4aa08f609283b94e6f29d35b2a7875e9994
Sven Neumann <sven.neumann@thinkject.de>
2018-01-25 06:19:28 +0100

    Basic sample using DataProtection API

---
+++ dotnet/DataProtection/DataProtectionConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataProtectionConsole {
+  public class Program {
+    public static void Main(string[] args) {
+      ServiceCollection services = new ServiceCollection();
+      services.AddDataProtection();
+
+      ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+      IDataProtectionProvider protectionProvider = serviceProvider.GetService<IDataProtectionProvider>();
+
+      IDataProtector protector = protectionProvider.CreateProtector("Sample");
+
+      Console.Write("Enter input: ");
+      string input = Console.ReadLine();
+
+      string protectedPayload = protector.Protect(input);
+      Console.WriteLine($"Protect returned: {protectedPayload}");
+
+      // unprotect the payload
+      string unprotectedPayload = protector.Unprotect(protectedPayload);
+      Console.WriteLine($"Unprotect returned: {unprotectedPayload}");
+
+      Console.ReadLine();
+    }
+  }
+}
